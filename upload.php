67dbf62b000ee7319623bc94aebcb88389e18e2a
Lucas <luc4snoga@gmail.com>
2016-09-11 00:25:54 +0200

    Projet permettant de voir le tracet d'un fichier gpx mais également d'y voir afficher des données xml

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+	var_dump($_FILES);
+
+  	if(isset($_FILES['file']))
+  	{
+    	$dossier = "cartes/";
+    	$fichier = 'map.gpx';
+
+    	if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier))
+    	{
+      		echo 'Upload avec succès !';
+			header('Location: map.html');
+			exit();
+    	}
+    	else
+    	{
+      		echo "Erreur vous n'avez pas mis de fichier, vous allez être redirigé dans 3 secondes";
+			header('Refresh: 3; url=index.html');
+    	}
+    }
+
+		
+
+ ?>
